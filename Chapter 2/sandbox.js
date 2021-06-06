4ffcd430a2179c60600105545bee6d8ef794831b
raphaelpavani <raphaelpavani@gmail.com>
2021-06-06 12:49:31 -0300

    Introduction: variables let, const var.

--- Chapter 2/sandbox.js
+++ Chapter 2/sandbox.js
@@ -1,3 +1,4 @@
-alert('Hello World');
-console.log('1');
-console.log('2');
+let age = 25; // jeito moderno modern way
+let year=2021; // jeito moderno modern way
+const point = 101; //imutável immutable
+var score = 100; //jeito antigo old way
